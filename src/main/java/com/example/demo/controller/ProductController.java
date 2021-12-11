3d48075e7ddd58415b3c2e141086cff69a67869b
ZHANGZEYU <indope755158932@gmail.com>
2021-12-11 13:33:44 +0800

    second commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,7 +4,9 @@ import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -12,10 +14,16 @@ import java.util.List;
 public class ProductController {
 
   @Autowired
-  ProductService productMannger;
+  ProductService productManager;
 
   @GetMapping("/products")
   public List<Product> getProducts() {
-    return productMannger.getProducts();
+    return productManager.getProducts();
+
+  }
+
+  @GetMapping("/products/{keyword}")
+  public List<Product> getProducts(@PathVariable ("keyword")String keyword){
+    return productManager.getProducts(keyword);
   }
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -22,11 +22,21 @@ public class ProductService {
 
   public List<Product> getProducts() {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION "
-          + "description"
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
           + " from PRODUCT";
 
       return connection.createQuery(query).executeAndFetch(Product.class);
+    }
+  }
+
+  public List<Product> getProducts(String keyword) {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
+          + " from PRODUCT where name like :keyword";
+
+      return connection.createQuery(query)
+          .addParameter("keyword", "%"+keyword+"%")
+          .executeAndFetch(Product.class);
     }
   }
 
